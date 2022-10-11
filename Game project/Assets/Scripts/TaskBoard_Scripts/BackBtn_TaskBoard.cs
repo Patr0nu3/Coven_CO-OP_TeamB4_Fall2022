89ceea997f9eaee00a0d06e665bb21b35d23b253
abbyder <abbyder393@gmail.com>
2022-10-10 23:56:03 -0400

    progress on taskboard

---
+++ Game project/Assets/Scripts/TaskBoard_Scripts/BackBtn_TaskBoard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackBtn_TaskBoard : MonoBehaviour
+{
+    public string LeaveTaskBoard = "AbbyTests";
+    
+    void OnMouseDown() {
+        // if (Input.GetMouseButtonDown(0)) {
+        Debug.Log("clicked");
+            SceneManager.LoadScene(LeaveTaskBoard);
+        // }
+    }
+    // void OnMouseDown()
+    // {
+    //     Debug.Log("clicked");
+    //     // Destroy the gameObject after clicking on it
+    //     // Destroy(gameObject);
+    // }
+}
