fb3a9edeff08e0d607e066d517b8eada91e897fb
abbyder <abbyder393@gmail.com>
2022-10-25 01:19:46 -0400

    item submit ui

---
+++ Game project/Assets/Scripts/ItemSubmitBtns.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSubmitBtns: MonoBehaviour
+{
+    public void MushroomSubmit() {
+        Debug.Log("mushroom submit");
+    }
+    
+    public void FlowerSubmit() {
+        Debug.Log("flower submit");
+    }
+    
+    public void BerriesSubmit() {
+        Debug.Log("berries submit");
+    }
+    
+    public void StarfishSubmit() {
+        Debug.Log("starfish submit");
+    }
+    
+    public void JellyfishSubmit() {
+        Debug.Log("jellyfish submit");
+    }
+    
+    public void HealthPotionSubmit() {
+        Debug.Log("healthpotion submit");
+    }
+    
+    public void GreenPotionSubmit() {
+        Debug.Log("greenpotion submit");
+    }
+    
+    public void BluePotionSubmit() {
+        Debug.Log("bluepotion submit");
+    }
+}
